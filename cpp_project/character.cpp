9d1313988954fd9db206308fbfbc1ba6650f62ee
brent <brent.gerets@gmail.com>
2022-11-13 16:36:56 +0100

    implemented character constructor and destructor

--- cpp_project/character.cpp
+++ cpp_project/character.cpp
@@ -2,5 +2,15 @@
 
 Character::Character()
 {
+}
 
+Character::Character(Coords *hitBox, Coords *position, unsigned int speed, unsigned int maxHealth, unsigned int currentHealth) :
+    hitBox(hitBox), position(position), speed(speed), maxHealth(maxHealth), currentHealth(currentHealth)
+{
+}
+
+Character::~Character()
+{
+    delete hitBox;
+    delete position;
 }
